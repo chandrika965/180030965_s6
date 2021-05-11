9215a95fb1260796ae1d757616744995bb8b3188
chandrika <chandrika@LAPTOP-EO6CP6N5>
2021-05-11 15:32:20 +0530

    loops

---
+++ Loops/src/practise/Pro1.java
@@ -0,0 +1,23 @@
+package practise;
+
+import java.util.*;
+public class Pro1 {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+
+	    int a=sc.nextInt();
+	    if(a>0){
+	        System.out.println("Positive");
+	    }
+	    else if(a==0)
+	    {
+	        System.out.println("Zero");
+	    }
+	    else
+	    {
+	        System.out.println("Negative");
+	    }
+	
+
+		}}
+}
